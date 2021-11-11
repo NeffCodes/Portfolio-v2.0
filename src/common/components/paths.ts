e2af58752a774e92fb85531f9f9b7a4a11a64516
Persigio <sparkraven@gmail.com>
2021-11-11 12:22:44 -0500

    Update paths.ts

--- src/common/components/paths.ts
+++ src/common/components/paths.ts
@@ -1,14 +1,17 @@
 export default {
     social: {
         github: { 
+            title: 'GitHub Repository',
             logo: 'github', 
             path: 'https://github.com/persigio',  
         },
         linkedin: { 
+            title: 'LinkedIn - James Neff',
             logo: 'linkedin', 
             path: 'https://www.linkedin.com/in/james-m-neff/',  
         },
         codepen: {
+            title: 'CodePen - Persigio',
             logo: 'codepen', 
             path: 'https://codepen.io/Persigio'
         }

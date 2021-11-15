03288e7050d357c05a479cc7c2f5e80bf58e8d6b
Persigio <sparkraven@gmail.com>
2021-11-15 16:45:36 -0500

    Update contact path in Nav to direct to current page contact section

--- src/common/components/nav/Nav.js
+++ src/common/components/nav/Nav.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { motion } from 'framer-motion'
-
+import { useRouter } from 'next/router'
 import logo from '../../../../public/assets/logo.svg'
 import styles from './Nav.module.css'
 
@@ -27,52 +27,54 @@ const item = {
 }
 
 export default function Nav(){
-    return (
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <Link href="/">
-            <motion.a
-              initial={{opacity:0}}
-              animate={{opacity:1}}
-              transition={{duration: 0.6}}
-            >
-              <div className={styles.logoContainer}>
-                <Image src={logo} alt="James Neff Logo" />
-              </div>
-            </motion.a>
-          </Link>
-          <div className={styles.linkContainer}>
-            <motion.ul 
-              initial="hidden"
-              animate="visible"
-              variants={list}
-              className={styles.list}
-            >
-              <motion.li variants={item}>
-                <Link href={{pathname:'/', hash: 'work'}}>
-                  <a className={styles.link}>
-                    Work
-                  </a>
-                </Link>
-              </motion.li>
-              <motion.li variants={item}>
-                <Link href={{pathname:'/', hash: 'about'}}>
-                  <a className={styles.link}>
-                    About
-                    {/* <div className={styles.underline}></div> */}
-                  </a>
-                </Link>
-              </motion.li>
-              <motion.li variants={item}>
-                <Link href={{pathname:'/', hash: 'contact'}}>
-                  <a className={styles.link}>
-                    Contact
-                  </a>
-                </Link>
-              </motion.li>
-            </motion.ul>
-          </div>
-        </nav>
-      </header>
-    )
+  const router = useRouter();
+
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link href="/">
+          <motion.a
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration: 0.6}}
+          >
+            <div className={styles.logoContainer}>
+              <Image src={logo} alt="James Neff Logo" />
+            </div>
+          </motion.a>
+        </Link>
+        <div className={styles.linkContainer}>
+          <motion.ul 
+            initial="hidden"
+            animate="visible"
+            variants={list}
+            className={styles.list}
+          >
+            <motion.li variants={item}>
+              <Link href={{pathname:'/', hash: 'work'}}>
+                <a className={styles.link}>
+                  Work
+                </a>
+              </Link>
+            </motion.li>
+            <motion.li variants={item}>
+              <Link href={{pathname:'/', hash: 'about'}}>
+                <a className={styles.link}>
+                  About
+                  {/* <div className={styles.underline}></div> */}
+                </a>
+              </Link>
+            </motion.li>
+            <motion.li variants={item}>
+              <Link href={{pathname: router.pathname, hash: 'contact'}}>
+                <a className={styles.link}>
+                  Contact
+                </a>
+              </Link>
+            </motion.li>
+          </motion.ul>
+        </div>
+      </nav>
+    </header>
+  )
 }

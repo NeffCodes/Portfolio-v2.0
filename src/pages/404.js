7f586c0bee729ec4c214a10103e5f6006461360d
Persigio <sparkraven@gmail.com>
2021-11-15 17:19:29 -0500

    Add layout to 404 page, create section containers

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import Layout from '../common/components/layout/Layout'
+
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <section className='sectionContainer'>
+        <h1 className='sectionContent'>404 - Page Not Found</h1>
+      </section>
+    </Layout>
+  )
+}

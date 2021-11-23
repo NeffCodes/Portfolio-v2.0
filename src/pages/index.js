fcf822e7bbd03e55ca1879baee817687a607cc7c
Persigio <sparkraven@gmail.com>
2021-11-23 17:06:48 -0500

    Fixed trihex causing screen expansion issue

--- src/pages/index.js
+++ src/pages/index.js
@@ -169,7 +169,7 @@ export default function Home() {
               </article>
             </a>
           </div>
-          <div className={styles.projectBkgdImage}></div>
+          <div className={styles.projectSideImage}></div>
         </div>  
       </section>
       

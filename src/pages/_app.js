c4d882e322d6bd95054fee79058c387b68c3c431
Persigio <sparkraven@gmail.com>
2022-01-11 16:10:18 -0500

    Setup GA
    Followed this article for setup: https://mariestarck.com/add-google-analytics-to-your-next-js-application-in-5-easy-steps/

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,8 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from '../common/lib/ga';
+
 import '../styles/globals.css';
 import GlobalLayout from '../common/components/layout/GlobalLayout';
 import DefaultLayout from '../common/components/layout/DefaultLayout';
@@ -5,6 +10,22 @@ import { AnimatePresence } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
   const Layout = Component.Layout || DefaultLayout;
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = url => {
+      ga.pageview(url);
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+    
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <AnimatePresence exitBeforeEnter >

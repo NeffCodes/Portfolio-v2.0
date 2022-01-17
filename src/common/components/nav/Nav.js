5d41115b57600d885fceb453c71543c452a5a685
Persigio <sparkraven@gmail.com>
2022-01-17 13:20:58 -0500

    Add #hero for when you click on logo to go back to top of homepage

--- src/common/components/nav/Nav.js
+++ src/common/components/nav/Nav.js
@@ -39,7 +39,7 @@ export default function Nav(){
             className={styles.container}
           >
           <li className={styles.logo_container}>
-            <Link href={{pathname:'/'}}>
+            <Link href={{pathname:'/', hash:'hero'}}>
               <motion.a
                 initial={{opacity:0}}
                 animate={{opacity:1}}

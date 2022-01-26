a508bc3675b57948ab7872b8cb48744565e0763a
Persigio <sparkraven@gmail.com>
2022-01-26 14:32:14 -0500

    Add entry motion to articleBody
    NOTE: need to figure out how to make it exit properly

--- src/common/components/articleBody/ArticleBody.js
+++ src/common/components/articleBody/ArticleBody.js
@@ -1,9 +1,29 @@
+import { motion } from 'framer-motion'
+
 import styles from "./ArticleBody.module.css"
+
+//motion variants
+let easing = [0.175, 0.85, 0.42, 0.96];
+
+const containerVariants = {
+  hidden: { y: 100, opacity: 0, transition: { duration: 0.5, ease: easing } },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: { delay: 0.1, duration: 0.5, ease: easing, staggerChildren: 0.2 }
+  }
+};
 
 export default function ArticleBody({ children }) {
   return (
-    <div className={styles.container}>
+    <motion.div 
+      className={styles.container}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
       {children}
-    </div>
+    </motion.div>
   )
 }

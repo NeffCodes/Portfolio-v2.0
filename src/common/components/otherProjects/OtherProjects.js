234dea6dafca27d1d963abbb188023e91db108d7
Persigio <sparkraven@gmail.com>
2022-01-26 15:31:49 -0500

    Add entry motion to otherProjects

--- src/common/components/otherProjects/OtherProjects.js
+++ src/common/components/otherProjects/OtherProjects.js
@@ -1,35 +1,92 @@
 import Link from 'next/link'
+import { motion, useAnimation } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+
 import { projectTileData } from '../../context/project-list'
 import { findTileLoopData } from '../../utils/tile-data-functions'
 
 import styles from "./OtherProjects.module.css"
 
+const containerVariants = {
+  visible: {
+    transition: {
+      delayChildren: .8,
+      staggerChildren: .3,
+    }
+  }
+}
+
+const linkVariants = {
+  initial: { 
+    y: -30, 
+    opacity: 0 
+  },
+  visible: { 
+    y: 0, 
+    opacity: 1, 
+    transition: { 
+      duration: 0.5, 
+      // delay: 1.5,
+      ease: [0.48, 0.15, 0.25, 0.96], 
+    } 
+  },
+  exit: {
+    y: -100,
+    opacity: 0,
+    transition: { duration: 0.2, ease: [0.48, 0.15, 0.25, 0.96] }
+  },
+};
+
+
 export default function OtherProjects({current}) {
   const tileData = findTileLoopData(projectTileData,current);
+  //hook to allow us to control the element in the screen view
+  const { inView, ref } = useInView(); 
+  //framer motion animation controls
+  const boxControls = useAnimation();
+
+  if(inView) {
+    boxControls.start("visible");
+    console.log("inview")
+  }
 
   return (
-    <div className={styles.container}>
+    <motion.div 
+      initial="initial"
+      animate={boxControls}
+      exit="exit"
+      variants={containerVariants}
+      className={styles.container} 
+    >
       <div className={styles.title_container}>
         <h2 className={styles.title}>Other Projects.</h2>
         <div className="line" /> 
       </div>
       
-      <div className={styles.link_container}>
-        <Link href={tileData.next.slug}> 
-          <article className={styles.link_card}>
-            <h3 className={styles.link_title}>{tileData.next.title}</h3>
-            <p>{tileData.next.description}</p>
-            <a className={styles.link}>View Project &rarr;</a>
-          </article>
-        </Link>
-        <Link href={tileData.nextnext.slug}> 
-          <article className={styles.link_card}>
-            <h3 className={styles.link_title}>{tileData.nextnext.title}</h3>
-            <p>{tileData.nextnext.description}</p>
-            <a className={styles.link}>View Project &rarr;</a>
-          </article>
-        </Link>
+      <div ref={ref} className={styles.link_container}>
+        <article className={styles.link_card}>
+          <motion.div variants={linkVariants}>
+            <Link href={tileData.next.slug}  variants={linkVariants}> 
+              <>
+                <h3 className={styles.link_title}>{tileData.next.title}</h3>
+                <p>{tileData.next.description}</p>
+                <a className={styles.link}>View Project &rarr;</a>
+              </>
+            </Link>
+          </motion.div>
+        </article>
+        <article className={styles.link_card}>
+        <motion.div variants={linkVariants}>
+          <Link href={tileData.nextnext.slug}> 
+            <>
+              <h3 className={styles.link_title}>{tileData.nextnext.title}</h3>
+              <p>{tileData.nextnext.description}</p>
+              <a className={styles.link}>View Project &rarr;</a>
+            </>
+          </Link>
+        </motion.div>
+        </article>
       </div>
-    </div>
+    </motion.div>
   )
 }

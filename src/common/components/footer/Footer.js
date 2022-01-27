53e812e8ff4339d56121c68db6a88a974f4d84a7
Persigio <sparkraven@gmail.com>
2022-01-27 16:50:10 -0500

    Add entry motion for footer
    - set up to repeat once in view using whileInView

--- src/common/components/footer/Footer.js
+++ src/common/components/footer/Footer.js
@@ -1,13 +1,46 @@
+import { motion } from 'framer-motion'
+
 import { externalPaths } from '../../context/external-path-list'
 import Socials from '../socials/Socials'
 import Copyright from '../copyright/Copyright'
 import styles from './Footer.module.css'
 
+//motion variants
+let easing = [0.175, 0.85, 0.42, 0.96];
+
+const containerVariants = {
+  hidden: { 
+    x: -50, 
+    opacity: 0, 
+    transition: { 
+      duration: 0.5, 
+      ease: easing 
+    } 
+  },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { 
+      delay: 0.1, 
+      duration: 0.5, 
+      ease: easing, 
+      delayChildren: .8,
+      staggerChildren: 0.2 
+    }
+  },
+};
+
 export default function Footer(){
   return (
     <footer>
       <div className={styles.container}>
-        <div className={styles.contact}>
+        <motion.div 
+          initial="hidden"
+          whileInView="visible"
+          exit="hidden"
+          variants={containerVariants}
+          className={styles.contact}
+        >
           <div id="contact" className={styles.text}>
             <Socials/>
             <h2> Say <span className='accent'>hello.</span></h2>
@@ -15,7 +48,7 @@ export default function Footer(){
             <a href={`mailto:${externalPaths.email}`} className={styles.email}>{externalPaths.email}</a>
           </div>
           <div className={styles.image}></div>
-        </div>
+        </motion.div>
           
       </div>
       <Copyright />

2d2937ae20b3316a182a0dec19acdb25fd94fa73
Elliot Gibson <elliot@elliotgibson.com>
2022-02-15 21:47:08 +0000

    Fixed scrolling issues :)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,26 +6,28 @@ import googleAnalytics from '../common/hooks/googleAnalytics';
 import GlobalLayout from '../common/components/layout/GlobalLayout';
 import '../styles/globals.css';
 
-const handleExitComplete = () => {
-  if(typeof window !== 'undefined'){
-    let hashID = window.location.hash
-    if(hashID) {
-      let element = document.querySelector(hashID);
-      if(element) { 
-        setTimeout( ()=> {
-          element.scrollIntoView({
-            behavior: 'smooth',
-            block:'start',
-            inline:'start',
-          })
-        }, 400)
-      } else {
-        window.scrollTo(0,0);
-        console.log('top')
-      }
-    }
-  }
-}
+//this was running while both the previous component and current component had been mounted which was causing weird issues :(
+//better to use a useEffect where you want to add navigation to hash :) -> see index.js
+// const handleExitComplete = () => {
+//   if(typeof window !== 'undefined'){
+//     let hashID = window.location.hash
+//     if(hashID) {
+//       let element = document.querySelector(hashID);
+//       if(element) { 
+//         setTimeout( ()=> {
+//           element.scrollIntoView({
+//             behavior: 'smooth',
+//             block:'start',
+//             inline:'start',
+//           })
+//         }, 400)
+//       } else {
+//         window.scrollTo(0,0);
+//         console.log('top')
+//       }
+//     }
+//   }
+// }
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
@@ -34,7 +36,8 @@ const MyApp = ({ Component, pageProps }) => {
   return (
     <GlobalLayout>
       <AnimatePresence 
-        onExitComplete={handleExitComplete}
+      exitBeforeEnter //ensures that all dom nodes of previous page have exited
+      //which was causing the scrolling to bottom
       >
         <Component {...pageProps} key={router.route}/>
       </AnimatePresence>

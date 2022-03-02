b6fa2785cad0b7f961b68144ba6e0c285ae615bd
Persigio <sparkraven@gmail.com>
2022-03-02 14:20:39 -0500

    Adding netlify form to footer

--- src/common/components/footer/Footer.js
+++ src/common/components/footer/Footer.js
@@ -44,8 +44,34 @@ export default function Footer(){
           <div id="contact" className={styles.text}>
             <Socials/>
             <h2> Say <span className='accent'>hello.</span></h2>
-            <p>Looking for a developer or designer to join your team? Feel free to reach out:</p>
-            <a href={`mailto:${externalPaths.email}`} className={styles.email}>{externalPaths.email}</a>
+            <p>Looking for a developer or designer to join your team? </p>
+            <p>Feel free to email me: <a href={`mailto:${externalPaths.email}`} className={styles.email}>{externalPaths.email}</a></p>
+            <p>Or send me a quick message here:</p>
+            
+            {/* <!-- A little help for the Netlify bots if you're not using a SSG --> */}
+            <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+              <input type="text" name="name" />
+              <input type="email" name="email" />
+              <textarea name="message"></textarea>
+            </form>
+
+
+            <form name="contact" method="post">
+              <input type="hidden" name="form-name" value="contact" />
+              <p>
+                <label>Your Name: <input type="text" name="name"/></label>
+              </p>
+              <p>
+                <label>Your Email: <input type="email" name="email"/></label>
+              </p>
+              <p>
+                <label>Message: <textarea name="message"></textarea></label>
+              </p>
+              <p>
+                <button type="submit">Send</button>
+              </p>
+            </form>
+
           </div>
           <div className={styles.image}></div>
         </motion.div>
